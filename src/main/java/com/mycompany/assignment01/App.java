5a98b06de1b5392386ebf10db92bbe24f58e2f28
6311236 <6311236@edu.vaniercollege.qc.ca>
2026-09-23 14:39:55 -0400

    Added the labels and fields and base root that will be needed as a base layout and then also labels/fields for the keyboard

--- src/main/java/com/mycompany/assignment01/App.java
+++ src/main/java/com/mycompany/assignment01/App.java
@@ -1,10 +1,13 @@
 package com.mycompany.assignment01;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -33,10 +36,53 @@ public class App extends Application {
     // shows the "1 of 6" count
     private Label counterLabel;
     
+    //value of the key pressed
+    private Label keyValueLabel;
     
+    // to show "not hqandled" later in red
+    private Label notHandledLabel;
+    
+    // will show the good keys being pressed
+    private Label correctLabel;
+    
+    // will show the incorrect keys being pressed
+    private Label incorrectLabel;
+    
+    // index of the current text that will be shown
+    private int currentIndex = 0;
+    
+    // to keep track of the count of good keys tha were pressed
+    private int correctCount = 0;
+    
+    // to keep track of the incorrect keys that were pressed
+    private int incorrectCount = 0;
 
     @Override
     public void start(Stage stage) {
+        
+        // OUR ROOT LAYOUT THATS GONNA BE NEEDED THROUGHOUT THE PROJECT
+        VBox root = new VBox(15);
+        root.setPadding(new Insets(15));
+        root.setAlignment(Pos.CENTER);
+        
+        
+        Label instructionLabel = new Label("Type the text below:");
+        
+        promptLabel = new Label(textLines[currentIndex]);
+        
+        typedField = new TextField();
+        typedField.setPrefWidth(500);
+        
+        counterLabel = new Label((currentIndex + 1) + " of " + textLines.length);
+        
+        // labels for info
+        
+        keyValueLabel = new Label("Key: ");
+        
+        notHandledLabel = new Label(); // will need to make like red
+        
+        correctLabel = new Label("Correct: 0");
+        incorrectLabel = new Label("Incorrect: 0");
         
     }
 
