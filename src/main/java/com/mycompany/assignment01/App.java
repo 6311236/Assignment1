d71982b969385dcf872c2f3002a19534400a6c72
6311236 <6311236@edu.vaniercollege.qc.ca>
2026-09-22 22:42:58 -0400

    Added text lines that will be needed to show on screen and the labels/textField for the instructions and typing area

--- src/main/java/com/mycompany/assignment01/App.java
+++ src/main/java/com/mycompany/assignment01/App.java
@@ -3,6 +3,7 @@ package com.mycompany.assignment01;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -11,16 +12,32 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
+    
+    private String[] textLines = {
+        
+        "Try typing this text. Do it as quickly and accurately as you can.",
+        "Next type another line of input data.",
+        "The quick brown fox jumps over the lazy dog.",
+        "Five big quacking zephyrs jolt my wax bed.",
+        "Sympathizing would fix Quaker objectives.",
+        "A large fawn jumped quickly over white zinc boxes."
+    };
+    
+    
+    //text the user must type
+    private Label promptLabel;      
+    
+    //what the user has typed
+    private TextField typedField;
+    
+    // shows the "1 of 6" count
+    private Label counterLabel;
+    
+    
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        
     }
 
     public static void main(String[] args) {
